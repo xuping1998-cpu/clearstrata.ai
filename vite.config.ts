ded298292eb42a75ad97831b98c92b230d88a620
xuping1998-cpu <xuping1998@gmail.com>
2026-07-06 10:11:20 -0700

    discussion2

--- vite.config.ts
+++ vite.config.ts
@@ -47,7 +47,7 @@ export default defineConfig({
         ],
       },
       workbox: {
-        maximumFileSizeToCacheInBytes: 4 * 1024 * 1024,
+        maximumFileSizeToCacheInBytes: 6 * 1024 * 1024,
         globPatterns: ['**/*.{js,css,html,ico,png,svg,jpg,jpeg}'],
         runtimeCaching: [
           {
